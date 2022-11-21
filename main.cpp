1dce0f8e03c7388d8809667499f007fe47dd938e
Andreeew99 <118534291+Andreeew99@users.noreply.github.com>
2022-11-21 11:20:08 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
-
+int main(){
+   float a,b,At,Aq,Ar;
+int i
+cin >>a >>b >>i
+switch (i) {
+   case(0)
+      At=(a*b)/2;
+cout << At;
+break;
+case (1)
+   Arq=a*a
+   cout << Aq;
+break;
+case (2)
+   Ar=a*b
+   cout << Ar;
+break;
+default :
+cout <<"opzione non valida";
 int main() {
    return 0;
 }
